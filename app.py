91205f03cf4f3d51443ef8cc8d853e797481a908
pyomelch <omelch.py@gmail.com>
2023-10-23 23:16:46 +0300

    fixed basic code template

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
-import json
-import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello Flask!!!"
+    return "Hello Flask!"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050, debug=True)
+app.run(host='0.0.0.0', port=5050, debug=True)
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5050, debug=True)
